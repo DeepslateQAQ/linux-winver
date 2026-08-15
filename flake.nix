98307ad1902ee8b8a415683fb620044ba202b887
deepslate <deepslate@localhost>
2026-08-15 10:47:25 +0800

    feat: :package: 实现了 Flakes 兼容，便于 Nix 用户导入环境

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "我修复了 Linux 非 KDE 桌面环境没有 winver 的 bug";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          winver = pkgs.stdenv.mkDerivation {
+            pname = "winver";
+            version = "0.1.0";
+
+            src = self;
+
+            nativeBuildInputs = [ pkgs.pkg-config pkgs.glib ];
+            buildInputs = [ pkgs.gtk4 ];
+
+            makeFlags = [ "PREFIX=$(out)" ];
+            installTargets = [ "install" ];
+
+            meta = {
+              description = "winver-style About dialog for Linux distributions, with a demo mode for 30 distros";
+              homepage = "https://github.com/deepslate/linux-winver";
+              license = nixpkgs.lib.licenses.mit;
+              platforms = nixpkgs.lib.platforms.linux;
+            };
+          };
+          default = self.packages.${system}.winver;
+        });
+    };
+}
